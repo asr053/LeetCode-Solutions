addba1f3716f8b7e5bea5f09d47c6cff5e0dc28a
Abhay Singh Raghuvanshi <75516843+asr053@users.noreply.github.com>
2023-02-21 08:10:24 +0530

    Time: 30 ms (55.07%), Space: 22.3 MB (93.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,15 +3,11 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         if(nums.size()==1)return nums[0];
         int l = 0 , h = nums.size()-1;
+        int add[]={1,-1};
         while(l<=h){
             int mid = (l+h)/2;
-            if(mid%2){
-                if(nums[mid]==nums[mid-1])l = mid+1;
+                if(nums[mid]==nums[mid+add[mid%2]])l = mid+1;
                 else h = mid-1;
-            }else{
-                if(nums[mid]==nums[mid+1]) l = mid+1;
-                else h = mid-1;
-            }
         }
         return nums[l];
     }
