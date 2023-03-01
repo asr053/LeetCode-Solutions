ce8548f943368ac4e1c4281de193db35acd1dc0d
Abhay Singh Raghuvanshi <75516843+asr053@users.noreply.github.com>
2023-03-01 17:58:09 +0530

    Time: 3 ms (44.11%), Space: 8.2 MB (7.81%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==1)return nums[0];
+      vector<vector<int>>dp(nums.size(),vector<int>(2,0));
+      dp[0][1]=nums[0];
+        //dp[i][0]->max sum till i when you do not rob at i 
+        //dp[i][1]->max sum till i when you rob at day i
+        for(int i=1;i<nums.size()-1;i++){
+            dp[i][0]=max(dp[i-1][0],dp[i-1][1]);
+            dp[i][1]=(dp[i-1][0]+nums[i]);
+        }
+      int val =  max(dp[nums.size()-2][0],dp[nums.size()-2][1]);
+        dp[0][0]=0 , dp[0][1]=0, dp[1][1]=nums[1],dp[1][0]=0;
+         for(int i=2;i<nums.size();i++){
+            dp[i][0]=max(dp[i-1][0],dp[i-1][1]);
+            dp[i][1]=(dp[i-1][0]+nums[i]);
+        }
+        val = max({val,dp[nums.size()-1][0],dp[nums.size()-1][1]});
+        return val;
+    }
+};
